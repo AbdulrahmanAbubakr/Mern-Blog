13d711aa5f7cb07abeef3822004aa72b91bc8420
moaaz <moaazabobakr69@gmail.com>
2024-04-20 23:14:26 +0200

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const env = require("dotenv");
 const app = express();
-const PORT = 3000;
+env.config();
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
