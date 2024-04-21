3d9c03d29496ce36d1b1cf0de6754537b844b408
moaaz <moaazabobakr69@gmail.com>
2024-04-21 11:42:03 +0200

    add middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -7,21 +7,26 @@ const authRoute = require("./routes/AuthRoute");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-
-
 mongoose
   .connect(process.env.MONGODB)
   .then(() => console.log("connected to mongodb"))
   .catch((err) => console.log(err));
 
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-
-
-
-
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
 
 // userRouter
-app.use('/api/user', userRoute)
+app.use("/api/user", userRoute);
 // authRouter
-app.use('/api/auth', authRoute)
+app.use("/api/auth", authRoute);
+// middleware and function to handle errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode|| 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message
+  })
+});
